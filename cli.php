d535550b2ccb1b285e5193a2189e1d7327854c54
Mariano Peterson <mariano@petersonpages.com>
2011-02-18 02:50:48 -0800

    Increased default days and limit, reworded help message.
    Increased default days and limit to show more data by default.
    If user's repo has so many commits that it increases SVN's XML
    response to a size that slows the tool down too much, the user
    can adjust by manually reducing the amount of data by setting
    flags like --days and --limit.

--- cli.php
+++ cli.php
@@ -4,30 +4,34 @@ require_once(dirname(__FILE__) . '/Slog.php');
 require_once(dirname(__FILE__) . '/CliParser.php');
 require_once(dirname(__FILE__) . '/Slog/Formatter/Interface.php');
 
-define("DEFAULT_DAYS", 14);
-define("DEFAULT_LIMIT",     1000);  // set null to remove limit
+define("DEFAULT_DAYS",  180);
+define("DEFAULT_LIMIT", 2000);  // set null to remove limit
 
 $cli = new CliParser();
 $cli->about('Wrapper around the SVN command line tool that provides '
                     . 'additional filtering capabilities such as '
                     . 'filtering by regex and author.')
     ->addOpt('a', 'author', 'Only show commits written by author. '
-                    . 'Set this repeatedly to cast a larger net.', false, CliParser::TYPE_ARRAY, ',')
+                    . 'Set repeatedly to cast a larger net.', false, CliParser::TYPE_ARRAY, ',')
     ->addOpt('c', 'color', 'Use color to style output.', false, CliParser::TYPE_FLAG)
-    ->addOpt('d', 'days', 'Number of days of data to fetch.')
-    ->addOpt('e', 'regex', "Only show commits that match regex. Set this "
+    ->addOpt('d', 'days', 'Number of days of data to fetch. '
+                    . 'Default is ' . DEFAULT_DAYS . '.')
+    ->addOpt('e', 'regex', "Only show commits that match regex. Set "
                     . "repeatedly to cast a larger net.\n"
-                    . "e.g., /component/")
-    ->addOpt('f', 'format', 'Format type: summary (default), oneline')
+                    . "e.g., /component/i")
+    ->addOpt('f', 'format', "Format type: summary, shortsummary, oneline.\n"
+                    . "Formats are defined in "
+                    . dirname(__FILE__) . "/Slog/Formatter/*")
     ->addOpt('h', 'help', 'Show usage.', false, CliParser::TYPE_FLAG)
     ->addOpt('i', 'ignore', 'Exclude commits written by author. '
                     . 'Set this repeatedly to cast a larger net.', false, CliParser::TYPE_ARRAY, ',')
-    ->addOpt('l', 'limit', 'Limit the number of commits to search')
+    ->addOpt('l', 'limit', 'Limit the number of commits to search. '
+                    . 'Default is ' . DEFAULT_LIMIT . '.')
     ->addOpt('r', 'repo', "SVN repository, e.g., http://svn.host.com/project.\n"
-                    . "If not specified but CWD is a working copy, the working "
-                    . "copy's repo will be used. If not in a working copy and "
-                    . "an environment variable named SLOG_REPO exists, its "
-                    . "value will be used.")
+                    . "Otherwise if the current directory is an SVN working "
+                    . "copy, the working copy's repo will be used.\n"
+                    . "Otherwise if the environment variable SLOG_REPO is "
+                    . "set, the SLOG_REPO variable will be used.")
     ->addOpt('s', 'reverse', 'Print the most recent commits first.', false, CliParser::TYPE_FLAG)
     ->addOpt('v', 'verbose', 'Print debugging information.', false, CliParser::TYPE_FLAG)
     ->load($argv);
